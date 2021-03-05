e958466c833194f93a1ef612ad8444147f58fc5d
Levi Kohler <kohler.la01@gmail.com>
2021-03-05 11:59:57 -0800

    write and pass seventh test

--- VendorTracker.Tests/ModelTests/VendorTests.cs
+++ VendorTracker.Tests/ModelTests/VendorTests.cs
@@ -6,13 +6,31 @@ using System;
 namespace VendorTracker.Tests
 {
   [TestClass]
-  public class VendorTests
+  public class VendorTests : IDisposable
   {
+
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+    
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
       Vendor newVendor = new Vendor("test vendor", "bakery");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+
+    [TestMethod]
+    public void GetId_ReturnsVendorId_Int()
+    {
+      string name = "Test Vendor";
+      string description = "Test description";
+      Vendor newVendor = new Vendor(name, description);
+
+      int result = newVendor.Id;
+
+      Assert.AreEqual(1, result);
     }
   }
 }
--- VendorTracker/Models/Vendor.cs
+++ VendorTracker/Models/Vendor.cs
@@ -18,5 +18,10 @@ namespace VendorTracker.Models
       Id = _instances.Count;
       Orders = new List<Order> { };
     }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
   }
 }
